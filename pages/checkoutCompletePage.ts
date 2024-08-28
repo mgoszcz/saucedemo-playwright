5f5f40d9bd56f88d5ae3bfeab8ff3536f351bf4a
Marcin Goszczynski <61630391+mgoszcz@users.noreply.github.com>
2024-08-28 16:13:02 +0200

    Tests implemented (#5)

--- pages/checkoutCompletePage.ts
+++ pages/checkoutCompletePage.ts
@@ -1,4 +1,27 @@
 import {BasePage} from "./basePage";
+import {Locator, Page} from "@playwright/test";
 
+const selectors = {
+    checkoutCompleteContainer: 'div.checkout_complete_container',
+    completeHeader: 'h2.complete-header',
+    completeText: 'div.complete-text',
+    completeImage: 'img.pony_express',
+    backHomeButton: 'button#back-to-products',
+}
 
-export class CheckoutCompletePage extends BasePage {}
+export class CheckoutCompletePage extends BasePage {
+    checkoutCompleteContainer: Locator;
+    completeHeader: Locator;
+    completeText: Locator;
+    completeImage: Locator;
+    backHomeButton: Locator;
+
+    constructor(page: Page) {
+        super(page);
+        this.checkoutCompleteContainer = page.locator(selectors.checkoutCompleteContainer);
+        this.completeHeader = this.checkoutCompleteContainer.locator(selectors.completeHeader);
+        this.completeText = this.checkoutCompleteContainer.locator(selectors.completeText);
+        this.completeImage = this.checkoutCompleteContainer.locator(selectors.completeImage);
+        this.backHomeButton = this.checkoutCompleteContainer.locator(selectors.backHomeButton);
+    }
+}
